6738f5556cb7260bd851328a5252768eec6464e0
Ben Smith <93447202+ben5049@users.noreply.github.com>
2024-02-03 23:57:13 +0000

    Created "display_demo" project, currently not working

---
+++ Firmware/STM32/main_board_v2/display_demo/Debug/Core/Startup/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (11.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+S_SRCS += \
+../Core/Startup/startup_stm32u5a9njhxq.s 
+
+S_DEPS += \
+./Core/Startup/startup_stm32u5a9njhxq.d 
+
+OBJS += \
+./Core/Startup/startup_stm32u5a9njhxq.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Startup/%.o: ../Core/Startup/%.s Core/Startup/subdir.mk
+	arm-none-eabi-gcc -mcpu=cortex-m33 -g3 -DDEBUG -DTX_SINGLE_MODE_NON_SECURE=1 -c -I"C:/Users/bens1/Documents/Other/Projects/Smart-Watch/Firmware/STM32/libraries/Inc" -I../Middlewares/ST/touchgfx/framework/include -I../TouchGFX/generated/fonts/include -I../TouchGFX/generated/gui_generated/include -I../TouchGFX/generated/images/include -I../TouchGFX/generated/texts/include -I../TouchGFX/generated/videos/include -I../TouchGFX/gui/include -I../Middlewares/ST/touchgfx_components/gpu2d/NemaGFX/include -I../Middlewares/ST/touchgfx_components/gpu2d/TouchGFXNema/include -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-sp-d16 -mfloat-abi=hard -mthumb -o "$@" "$<"
+
+clean: clean-Core-2f-Startup
+
+clean-Core-2f-Startup:
+	-$(RM) ./Core/Startup/startup_stm32u5a9njhxq.d ./Core/Startup/startup_stm32u5a9njhxq.o
+
+.PHONY: clean-Core-2f-Startup
+
